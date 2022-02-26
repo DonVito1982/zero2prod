7bfbc9d3a290554ff22c85ef3535c7e2ebfe21a0
Víctor Zambrano <donvito4571@gmail.com>
2022-02-25 20:26:01 -0400

    Fix fmt offense

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,9 @@ async fn health_check() -> impl Responder {
 }
 
 pub fn run() -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8000")?
-    .run();
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .bind("127.0.0.1:8000")?
+        .run();
 
     Ok(server)
 }
