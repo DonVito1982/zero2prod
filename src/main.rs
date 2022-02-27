19b82c7ae604d18965526c1726142b94c7015f0f
Víctor Zambrano <donvito4571@gmail.com>
2022-02-26 20:28:08 -0400

    Add port finding logic

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use std::net::TcpListener;
 use zero2prod::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    run("127.0.0.1:8000")?.await
+    let fail_text = "Failed to bind random port";
+    let listener = TcpListener::bind("127.0.0.1:8000").expect(fail_text);
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,23 +1,28 @@
+use std::net::TcpListener;
+
 #[actix_web::test]
 async fn health_check_works() {
     // Arrange
-    spawn_app();
-
-    // We need to bring in `reqwest`
-    // to perform HTTP resquests against our application.
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
+    // Act
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
+
     // Assert
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = zero2prod::run("127.0.0.1:0").expect("Failed to bind address");
+fn spawn_app() -> String {
+    let fail_text = "Failed to bind random port";
+    let listener = TcpListener::bind("127.0.0.1:0").expect(fail_text);
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
     let _ = actix_web::rt::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
